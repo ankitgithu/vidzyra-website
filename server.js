eb470ce189506c5bc1f394112559f9712d30a710
Ankit Singh <akrp1432@gmail.com>
2026-04-08 21:49:37 +0530

    Fix PORT for Render

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
 
+
 dotenv.config();
 
 const app = express();
@@ -114,6 +115,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 // 🔥 server start
-app.listen(5000, () => {
-  console.log("Server running on port 5000 🚀");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
